6975dd782c644a63e3cd7e54ca78d4de3e53c813
Pete de Zwart <dezwart@gmail.com>
2020-05-08 10:15:17 +1000

    - Revert back to actual homer text.

--- bin/homer.sh
+++ bin/homer.sh
@@ -2,22 +2,22 @@
 
 homers_texts=~/share/odyssey.txt
 
-dadadodo -c 1 -w 1048576 $homers_texts 2> /dev/null
+#dadadodo -c 1 -w 1048576 $homers_texts 2> /dev/null
 
-#declare -a lines
+declare -a lines
 
-#i=0
+i=0
 
-#old_ifs="$IFS"
-#IFS=$'\n'
+old_ifs="$IFS"
+IFS=$'\n'
 
-#for line in `cat ~/share/odyssey.txt`
-#do
-#    lines[$i]="$line"
-#    let i+=1
-#done
+for line in `cat ~/share/odyssey.txt`
+do
+    lines[$i]="$line"
+    let i+=1
+done
 
-#index=$(($RANDOM*$RANDOM%$i))
-#line="${lines[$index]}"
+index=$(($RANDOM*$RANDOM%$i))
+line="${lines[$index]}"
 
-#echo "$line"
+echo "$line"
